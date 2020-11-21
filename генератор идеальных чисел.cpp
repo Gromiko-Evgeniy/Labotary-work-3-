7521d2813279d286d54522121d975d81c9090b3a
Gromiko-Evgeniy <74821242+Gromiko-Evgeniy@users.noreply.github.com>
2020-11-21 14:55:43 +0200

    Add files via upload

---
+++ генератор идеальных чисел.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+
+    for ( long long i = 0; i < 1000000000; i++)
+    {
+        int s = 1;
+        for (int j = 2; j < i; j++) 
+        {
+            if (i % j == 0) 
+            {
+                s += j;
+            }
+        }
+        if (s == i) 
+        {
+            cout << i << " ";
+        }
+    }
+
+
+}
